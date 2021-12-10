0e89341b91748ed208a6689fb809cb2c3d5b6fe5
brianbacca <brianbn05@gmail.com>
2021-12-10 15:22:02 -0300

    commit

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,14 @@
 // Import the controllers functions
-const registerUser = require('./registerUser.js');
-const validateToken = require('./validateToken.js');
+const registerUser = require("./registerUser.js");
+const validateToken = require("./validateToken.js");
+const getCategories = require("./getCategories");
+const getLabels = require("./getLabels");
 // ...
 
 module.exports = {
-	registerUser,
-	validateToken,
-	// ...
-}
+  registerUser,
+  validateToken,
+  getCategories,
+  getLabels,
+  // ...
+};
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,15 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Obtenemos la lógica correspondiente desde controllers/index.js
-//const { registerUser } = require('../controllers');
+const { getCategories } = require("../controllers");
 
-// - - GET /register
-// Ejecuta la ruta /register
-// ruta relativa!
-router.get('/',async (req,res) => {
-	/* let result = await registerUser(req.body);
-	res.status(result.status).json(result); */
+
+router.get("/", async (req, res) => {
+  let result = await getCategories();
+  res.json(result);
 });
 
 module.exports = router;
