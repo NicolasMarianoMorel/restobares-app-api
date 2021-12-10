dae742d7c5ea22deaca7c13f92c50abc6061f7cc
Smesaz <semesile@gmail.com>
2021-12-10 16:05:01 -0500

    updated with Brian and Elias branches

--- src/routes/resto/table/menu.js
+++ src/routes/resto/table/menu.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {menu} = require('../../../controllers/menu.js');
+const {menu} = require('../../../controllers/getmenu.js');
 
 router.get('/',async (req,res) => {
 	try{
