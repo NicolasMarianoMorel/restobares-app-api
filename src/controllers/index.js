785ceb31801e3d26aa2d60f7302935bd1bfbb394
NicolasMarianoMorel <nicolasmorel77@gmail.com>
2021-12-10 22:09:22 -0300

    dejame en paz joputa#

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,33 +1,10 @@
 // Import the controllers functions
-<<<<<<< HEAD
-const registerUser = require('./registerUser.js');
-const validateToken = require('./validateToken.js');
-const getOrders = require('./getOrders.js');
-=======
-
 const registerUser = require("./registerUser.js");
 const validateToken = require("./validateToken.js");
->>>>>>> c2a8c749cf709ef739b9fee31c9041e468f10ab8
 const getCategories = require("./getCategories");
 const getLabels = require("./getLabels");
 const getUser = require("./getUser");
 const getDiscounts = require("./getDiscounts");
-<<<<<<< HEAD
-const postOrder = require('./postOrder.js');
-// ...
-
-module.exports = {
-	registerUser,
-	validateToken,
-	getOrders,
-	postOrder,
-  getCategories,
-  getLabels,
-  getUser,
-  getDiscounts,
-	// ...
-}
-=======
 const getOrders = require('./getOrders.js');
 const postOrder = require('./postOrder.js');
 // ...
@@ -44,4 +21,3 @@ module.exports = {
   // ...
 };
 
->>>>>>> c2a8c749cf709ef739b9fee31c9041e468f10ab8
