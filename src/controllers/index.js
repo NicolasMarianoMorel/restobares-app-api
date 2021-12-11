da4e1aaffa68d450d289ba3c15fb1ed3ba32a8f8
eliasdbr <eliasdbr@outlook.com>
2021-12-10 22:32:24 -0300

    Elias/ Pull from Pibes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,22 +1,4 @@
 // Import the controllers functions
-<<<<<<< HEAD
-<<<<<<< HEAD
-const registerUser = require('./registerUser.js');
-const validateToken = require('./validateToken.js');
-const getOrders = require('./getOrders.js');
-const postOrder = require('./postOrder.js');
-// ...
-
-module.exports = {
-	registerUser,
-	validateToken,
-	getOrders,
-	postOrder,
-	// ...
-}
-=======
-=======
->>>>>>> 738b691e16a1fce1c3bd3c9337033c67a468689a
 const registerUser = require("./registerUser.js");
 const validateToken = require("./validateToken.js");
 const getCategories = require("./getCategories");
@@ -40,8 +22,3 @@ module.exports = {
   getMenu,
   // ...
 };
-<<<<<<< HEAD
->>>>>>> d17c34a7d119aad44e28d751609d49a78e9f348c
-=======
-
->>>>>>> 738b691e16a1fce1c3bd3c9337033c67a468689a
