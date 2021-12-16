70ea3a1d3f55478b16ddf363056a103778a134b9
NicolasMarianoMorel <nicolasmorel77@gmail.com>
2021-12-16 17:27:33 -0300

    Ruta GetFeedback e inicio hardcodeado

--- src/controllers/getOrdersStaff.js
+++ src/controllers/getOrdersStaff.js
@@ -2,12 +2,9 @@
 var { usersTables } = require('../cache.js');
 
 module.exports = function(idResto) {
-	// Va a buscar al usuario idResto a través del numero de mesa de idTable
-	// Le restamos 1 porque idTable arranca de 1 y la posición del array
-	// arranca en 0.
-    let tables = usersTables[idResto].tables
+ let tables = usersTables[idResto].tables
     
-//agregar logica para q no devuelva las mesas q no tienen ordenes
+//solo mapeo las mesas que tengan algun producto en su currentOrder
     let currentsOrders = tables.filter(el => el.currentOrder.time !== "").map(el => {
         return {
             idTable: el.tableId,
