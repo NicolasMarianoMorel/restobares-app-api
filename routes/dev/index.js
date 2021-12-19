ea227964a937671b4a67bb432a3efe8aaa6f3d37
eliasdbr <eliasdbr@outlook.com>
2021-12-19 15:38:12 -0300

    Elias/Agregada ruta GET /dev/users

--- routes/dev/index.js
+++ routes/dev/index.js
@@ -4,12 +4,14 @@ var router = express.Router();
 
 // Import routers
 const clear = require('./clear.js');
+const users = require('./users.js');
 // ...
 
 // Configurar routers 
 router.use('/clear',(req,res,next) => {
      next();
 } , clear);
+router.use('/users',users);
 // ...
 
 module.exports = router;
