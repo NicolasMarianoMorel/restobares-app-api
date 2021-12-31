4aff88594942911ca27821f933e96a6e6cd7e594
eliasdbr <eliasdbr@outlook.com>
2021-12-31 12:53:06 -0300

    Elias/CORS Permissions enabled.

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ const storage = multer.diskStorage({
 });
 app.use(multer({ storage }).single("image"));
 
-//app.use((req, res, next) => {
-//	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//	res.header('Access-Control-Allow-Credentials', 'true');
-//	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//	next();
-//});
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
 app.use(cors());
 
