4af180916da13457e0062f3f7275baf88765109c
eliasdbr <eliasdbr@outlook.com>
2022-01-12 23:57:25 -0300

    Elias / Fix switching state from pay_online to pay_cash

--- controllers/sendPayment.js
+++ controllers/sendPayment.js
@@ -4,7 +4,8 @@ const {usersTables} = require("../cache.js");
 module.exports = function (idResto,idTable,state,tip) {
 	// Change the state of the table to the one recieved through the body
 	if (!state) throw new Error('You must specify a state.');
-	if (usersTables[idResto].tables[idTable - 1].state !== "eating")
+	if (usersTables[idResto].tables[idTable - 1].state !== "eating" &&
+			usersTables[idResto].tables[idTable - 1].state !== "pay_online")
 		throw new Error('You have a pending order. Please wait until it is delivered.');
 	usersTables[idResto].tables[idTable - 1].state = state;
 	usersTables[idResto].tables[idTable - 1].tip = tip || 0;
